c976c810ce2c95aeffa39f959f0109a54eb9fa1e
Clarence Grady <clarence.grady@ui.ac.id>
2023-11-22 18:16:53 +0700

    starting point

---
+++ lib/screens/home.dart
@@ -0,0 +1,63 @@
+import 'package:bookmate/widgets/right_drawer.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Game Inventory',
+        ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      endDrawer: const RightDrawer(),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'Game Inventory', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card kita.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    }
+}
---
+++ lib/screens/register.dart
@@ -0,0 +1,109 @@
+import 'dart:convert';
+
+import 'package:bookmate/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  // final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+  final request = context.watch<CookieRequest>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Form(
+        // key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(labelText: 'Confirm Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please confirm your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 24.0),
+              ElevatedButton(
+                child: Text('Register'),
+                onPressed: () async {
+                  String username = _usernameController.text;
+                  String password1 = _passwordController.text;
+                  String password2 = _confirmPasswordController.text;
+
+                  // Cek kredensial
+                  // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                  // Untuk menyambungkan Android emulator dengan Django pada localhost,
+                  // gunakan URL http://10.0.2.2/
+                  final response = await request.postJson("http://:)/", jsonEncode({
+                    "username": username,
+                    "password1": password1,
+                    "password2":password2,
+                  }));
+                  if (response['status']=='success'){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Successfully registered!'),
+                      ),
+                    );
+                    Navigator.pushReplacement(
+                      context, 
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                  }
+                  else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Failed to register!'),
+                      ),
+                    );
+                  }
+
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
