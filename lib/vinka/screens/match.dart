d1b31f93c1a44560c9f9a339c2425c66e2036178
vinkakniv <vinka.aeris@gmail.com>
2023-12-16 00:28:48 +0700

    fix match card

--- lib/vinka/screens/match.dart
+++ lib/vinka/screens/match.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:bookmate/globals.dart' as globals; 
-
+import 'package:bookmate/globals.dart' as globals;
 
 class MatchPage extends StatefulWidget {
   MatchPage({Key? key}) : super(key: key);
@@ -57,100 +56,187 @@ class _MatchPageState extends State<MatchPage> {
     final request = context.watch<CookieRequest>();
 
     return Center(
-        child: currentIndex < cards.length
-            ? GestureDetector(
-                onHorizontalDragEnd: (details) {
-                  if (details.primaryVelocity! > 0) {
-                    // Swiped right
-                    fetchNextCard(request);
-                  } else if (details.primaryVelocity! < 0) {
-                    // Swiped left
-                    fetchNextCard(request);
-                  }
-                },
-                child: Card(
-                  color: Theme.of(context).colorScheme.surfaceVariant,
-                  margin: const EdgeInsets.all(16.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: const EdgeInsets.all(16.0),
-                    alignment: Alignment.centerLeft, // Add this line
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start, // Add this line
-                      children: [
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(cards[currentIndex]['picture']),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          currentIndex < cards.length
+              ? GestureDetector(
+                  onHorizontalDragEnd: (details) {
+                    if (details.primaryVelocity! > 0) {
+                      // Swiped right
+                      fetchNextCard(request);
+                    } else if (details.primaryVelocity! < 0) {
+                      // Swiped left
+                      fetchNextCard(request);
+                    }
+                  },
+                  child: Card(
+                    color: Theme.of(context).colorScheme.surfaceVariant,
+                    margin: const EdgeInsets.all(16.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height * 0.58,    
+                      padding: const EdgeInsets.all(16.0),
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 27.0),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(cards[currentIndex]['picture']),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 16.0),
-                        Text(
-                          cards[currentIndex]['name'],
-                          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 12.0),
-                        Text(
-                          cards[currentIndex]['bio'],
-                          style: const TextStyle(fontSize: 20, fontStyle: FontStyle.italic, color: Color.fromARGB(255, 80, 80, 80)),
-                        ),
-                        const SizedBox(height: 16.0),
-                        const Text(
-                          'Interest: ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.black,
+                          const SizedBox(height: 20.0),
+                          Text(
+                            cards[currentIndex]['name'],
+                            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        RichText(
-                          textAlign: TextAlign.left,
-                          text: TextSpan(
-                            children: List.generate(
-                              cards[currentIndex]['interest'].length,
-                              (index) {
-                                return WidgetSpan(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                                    child: Container(
-                                      padding: const EdgeInsets.only(left: 10, right: 10, bottom: 5),
-                                      decoration: BoxDecoration(
-                                        color: const Color(0xFFB6536B),
-                                        borderRadius: BorderRadius.circular(16.0),
-                                      ),
-                                      child: Text(
-                                        cards[currentIndex]['interest'][index].toString(),
-                                        style: const TextStyle(
-                                          fontSize: 13,
-                                          color: Colors.white,
+                          const SizedBox(height: 20.0),
+                          Text(
+                            cards[currentIndex]['bio'],
+                            style: const TextStyle(fontSize: 20, fontStyle: FontStyle.italic, color: Color.fromARGB(255, 80, 80, 80)),
+                          ),
+                          const SizedBox(height: 30.0),
+                          const Text(
+                            'Interest: ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          RichText(
+                            textAlign: TextAlign.left,
+                            text: TextSpan(
+                              children: List.generate(
+                                cards[currentIndex]['interest'].length,
+                                (index) {
+                                  return WidgetSpan(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(top: 4.0, bottom: 4.0, right: 4.0),
+                                      child: Container(
+                                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 5),
+                                        decoration: BoxDecoration(
+                                          color: const Color(0xFFB6536B),
+                                          borderRadius: BorderRadius.circular(16.0),
+                                        ),
+                                        child: Text(
+                                          cards[currentIndex]['interest'][index].toString(),
+                                          style: const TextStyle(
+                                            fontSize: 13,
+                                            color: Colors.white,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                );
-                              },
+                                  );
+                                },
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 12.0),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              )
-            : const Text('No matches available'),
+                )
+              : const Text('No matches available'),
+                const SizedBox(height: 5.0), // Add spacing between card and buttons
+                  Container(
+                    alignment: Alignment.center,
+                    child: 
+                    Column(children: [
+                      ElevatedButton.icon(
+                                onPressed: () {
+                                },
+                                icon: const Icon(
+                                  Icons.person,
+                                  color: Colors.white, // Warna ikon dalam tombol
+                                ),
+                                label: const Text(
+                                  'See Profile',
+                                  style: TextStyle(
+                                    color: Colors.white, // Warna teks dalam tombol
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black, // Warna tombol untuk Swipe Kiri
+                                ),
+                              ),
+                          Row(
+                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton.icon(
+                                onPressed: () {
+                                  fetchNextCard(request);
+                                  setState(() {
+                                  });
+                                },
+                                icon: const Icon(
+                                  Icons.arrow_circle_left_outlined,
+                                  color: Colors.white, // Warna ikon dalam tombol
+                                ),
+                                label: const Text(
+                                  'Swipe Kiri',
+                                  style: TextStyle(
+                                    color: Colors.white, // Warna teks dalam tombol
+                                  ),
+                                ),      
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black, // Warna tombol untuk Swipe Kiri
+                                ),
+                              ),
+
+                              ElevatedButton.icon(
+                                onPressed: () async {
+                                  final response = await request.post("${globals.domain}/match/accept-flutter/",
+                                  jsonEncode(<String, String>{
+                                    "name" : cards[currentIndex]['name'],
+                                    "bio" : cards[currentIndex]['bio'],
+                                    "matching_id" : cards[currentIndex]['matchingId'],
+                                    'id' : cards[currentIndex]['userId'],
+                                }));
+                                // print(response['status']);
+                                if (response['status'] == 'success') {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(const SnackBar(
+                                    content: Text("Explore Your Favorite Books Together!"),
+                                    ));
+                                }
+                                  fetchNextCard(request);
+                                  setState(() {
+                                  });
+                                },
+                                icon: const Icon(
+                                  Icons.favorite,
+                                  color: Colors.white, // Warna ikon dalam tombol
+                                ),
+                                label: const Text(
+                                  'Swipe Kanan',
+                                  style: TextStyle(
+                                    color: Colors.white, // Warna teks dalam tombol
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFB6536B), // Warna tombol untuk Swipe Kiri   
+                                ),
+                              ),
+                            ],
+                          ),
+                    ]),
+                    )
+        ],
+      ),
     );
   }
 }
-
-
-
-
